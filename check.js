52b8bc261345e17e044b74fe1d20edaaf2420678
sabrinasaturi <sabrinasaturi3@gmail.com>
2026-05-22 16:35:51 -0300

    build: configure types and fix API route mapping
    - Added vite/client types to tsconfig
    - Corrected Vercel API rewrite destination
    - Removed obsolete diagnostic check script

--- check.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-let content = fs.readFileSync('server.pg.ts', 'utf8');
-
-// The file doubled in size because maybe my regex replaced poorly?
-// Let's check how many times setupDatabase is there.
-console.log("Lines: ", content.split('\\n').length);
-console.log("Imports pg: ", content.match(/import \{ Pool \} from 'pg'/g));
